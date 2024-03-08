629a391abf328cad336e19d4185506a1b5c234be
Fonyuy Gita <FonyuyJudegita@gmail.com>
2024-03-08 04:27:10 -0800

    added auth page container wrapper  DAY:1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,31 @@
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { LoginButton } from "@/components/auth/login-button";
+
+const font=Poppins({
+  subsets:["latin"],
+  weight:["600"]
+})
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[teal]">
-      <h2 className="text-white">WELCOME TO THE HOME PAGE</h2>
+    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+     <div className="space-y-6 text-center">
+      <h1 className={cn("text-6xl font-semibold text-white drop-shadow-md", font.className,)}>🔐 AUTH MASTERCLASS</h1>
+      <p className="text-white text-2xl ">Next authentication masterclass</p>
+
+      <div>
+        <LoginButton>
+
+      <Button variant='secondary' size="lg">Sign in 📖</Button>
+
+      </LoginButton>
+
+
+      </div>
+     </div>
       
     </main>
   );
